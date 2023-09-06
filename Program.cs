7ff3414b03685e75c9c63d23ab0f559cf03c9cad
Reidar Nilsen <4reidar@gmail.com>
2023-09-06 15:50:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloEdugrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            DateTime currentDate = DateTime.Now;
+            Console.WriteLine($"Hej, {name}, på {currentDate:d}");
+        }
+    }
+}
